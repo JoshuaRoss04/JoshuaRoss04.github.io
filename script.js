1ae602695b2fd842cce02c0dab34538f8767811c
JoshuaRoss04 <83415342+JoshuaRoss04@users.noreply.github.com>
2021-05-26 14:46:45 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+var somme = 0
+ 
+for (let i = 0; i < 5; i++) {
+  var num = parseInt(prompt("Choisisez un nombre"))
+  socument.write (num)
+  somme = somme + num
+}
+document.write ("votre reponse est " +somme)
+ 
